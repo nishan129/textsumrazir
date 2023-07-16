bb296b8050dc0bc5ba207451b4118a2204900803
Nishant Borkar <107128913+nishan129@users.noreply.github.com>
2023-07-16 11:29:21 +0530

    Add files via upload

---
+++ src/textsummarizer/pipeline/stage_04_model_trainer_pipeline.py
@@ -0,0 +1,16 @@
+from textsummarizer.config.configuration import ConfigurationManager
+from textsummarizer.conponents.model_trainer import ModelTrainer
+from textsummarizer.logging import logger
+
+
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+    
+    def main(self):
+        config = ConfigurationManager()
+        model_trainer_config = config.get_model_trainer_config()
+        model_trainer_config = ModelTrainer(config=model_trainer_config)
+        model_trainer_config.train()
+
